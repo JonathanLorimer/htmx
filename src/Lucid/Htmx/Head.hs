e61761e3667826392dbebcf35a5bba8a1d4ba84a
Jonathan Lorimer <32466011+JonathanLorimer@users.noreply.github.com>
2024-07-27 15:07:16 -0400

    feat: update head utils to reflect v2 extension urls (#7)

--- src/Lucid/Htmx/Head.hs
+++ src/Lucid/Htmx/Head.hs
@@ -6,7 +6,18 @@ This module defines utilities for installing HTMX and HTMX extensions
 via the head tag in your html document
 <https://htmx.org/docs/#installing>
 -}
-module Lucid.Htmx.Head where
+module Lucid.Htmx.Head (
+    useHtmx,
+    useHtmxVersion,
+    useHtmxExtension,
+    useHtmxExtensionV,
+    useHtmxExtensions,
+    useHtmxExtensionsV,
+    recommendedVersion,
+    htmxSrc,
+    htmxSrcWithSemVer,
+    htmxExtSrc
+) where
 
 import Data.Foldable (forM_)
 import Data.Text (Text, pack)
@@ -16,42 +27,33 @@ import Lucid.Base (Attribute, makeAttribute)
 import Lucid.Htmx.Extension
 import Lucid.Htmx.Render
 
--- | Place in your @head_@ tag to use htmx attributes in your lucid template
-useHtmx :: (Monad m) => HtmlT m ()
-useHtmx = script_ [src_ htmxSrc] ("" :: Html ())
 
 -- | Place in your template after @useHtmx@, but before where the extension is used via @hxExt_@
-useHtmxExtension :: (Monad m) => Text -> HtmlT m ()
-useHtmxExtension ext = script_ [src_ $ htmxSrc <> extensionPath ext] ("" :: Html ())
+-- NOTE: This uses 'recommendedVersion' as the version section of the URL
+useHtmxExtension :: (Monad m) => HtmxExtension -> HtmlT m ()
+useHtmxExtension = useHtmxExtensionV recommendedVersion
+
+-- | Same as 'useHtmxExt' but lets you choose the version url
+useHtmxExtensionV :: (Monad m) => (Natural, Natural, Natural) -> HtmxExtension -> HtmlT m ()
+useHtmxExtensionV v ext = script_ [src_ $ htmxExtSrc v (render ext)] ("" :: Html ())
 
 -- | A typesafe version of 'useHtmxExtension' based on the "included" extensions
 -- that the htmx codebase is tested against
-useHtmxExt :: (Monad m) => HtmxExtension -> HtmlT m ()
-useHtmxExt ext = script_ [src_ $ htmxSrc <> extensionPath (render ext)] ("" :: Html ())
-
--- | Same as 'useHtmxExt' but with a versioned url
-useHtmxExtV :: (Monad m) => (Natural, Natural, Natural) -> HtmxExtension -> HtmlT m ()
-useHtmxExtV v ext = script_ [src_ $ htmxSrcWithSemVer v <> extensionPath (render ext)] ("" :: Html ())
-
--- | A typesafe version of 'useHtmxExtension' based on the "included" extensions
--- that the htmx codebase is tested against
-useHtmxExts :: (Monad m) => [HtmxExtension] -> HtmlT m ()
-useHtmxExts exts = forM_ exts useHtmxExt
+-- NOTE: This uses 'recommendedVersion' as the version section of the URL
+useHtmxExtensions :: (Monad m) => [HtmxExtension] -> HtmlT m ()
+useHtmxExtensions exts = forM_ exts useHtmxExtension
 
 -- | Same as 'useHtmxExts' but with a versioned url
-useHtmxExtsV :: (Monad m) => (Natural, Natural, Natural) -> [HtmxExtension] -> HtmlT m ()
-useHtmxExtsV v exts = forM_ exts (useHtmxExtV v)
+useHtmxExtensionsV :: (Monad m) => (Natural, Natural, Natural) -> [HtmxExtension] -> HtmlT m ()
+useHtmxExtensionsV v exts = forM_ exts (useHtmxExtensionV v)
+
+-- | Place in your @head_@ tag to use htmx attributes in your lucid template
+useHtmx :: (Monad m) => HtmlT m ()
+useHtmx = useHtmxVersion recommendedVersion
 
 -- | Choose the version of htmx to use using a 3-tuple representing semantic versioning
 useHtmxVersion :: (Monad m) => (Natural, Natural, Natural) -> HtmlT m ()
 useHtmxVersion semVer = script_ [src_ $ htmxSrcWithSemVer semVer] ("" :: Html ())
-
--- | Choose the version of a htmx extension you want to use.
--- Should only be used when using @useHtmxVersion@ and the semantic version should be the same
-useHtmxVersionExtension ::
-    (Monad m) => (Natural, Natural, Natural) -> Text -> HtmlT m ()
-useHtmxVersionExtension semVer ext =
-    script_ [src_ $ htmxSrcWithSemVer semVer <> extensionPath ext] ("" :: Html ())
 
 -- | This is the recommended version of htmx for using this library
 -- (lucid-htmx). It is the version of the documentation that the implementation
@@ -65,15 +67,23 @@ htmxSrc = "https://unpkg.com/htmx.org"
 showT :: (Show a) => a -> Text
 showT = pack . show
 
-htmxSrcWithSemVer :: (Natural, Natural, Natural) -> Text
-htmxSrcWithSemVer (major, minor, patch) =
-    htmxSrc
-        <> "@"
-        <> showT major
-        <> "."
-        <> showT minor
-        <> "."
-        <> showT patch
+showSemVer :: (Natural, Natural, Natural) -> Text
+showSemVer (major, minor, patch) = "@"
+    <> showT major
+    <> "."
+    <> showT minor
+    <> "."
+    <> showT patch
 
-extensionPath :: Text -> Text
-extensionPath ext = "/dist/ext/" <> ext <> ".js"
+htmxSrcWithSemVer :: (Natural, Natural, Natural) -> Text
+htmxSrcWithSemVer ver =
+    htmxSrc <> showSemVer ver
+
+htmxExtSrc :: (Natural, Natural, Natural) -> Text -> Text
+htmxExtSrc ver ext = 
+    "https://unpkg.com/htmx-ext-" 
+    <> ext 
+    <> showSemVer ver 
+    <> "/"
+    <> ext
+    <> ".js"
